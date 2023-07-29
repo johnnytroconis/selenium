7be5b2f11d41500f86f493b880b90726ec39ec09
johnnytroconis <johalex530@gmai.com>
2023-07-29 16:01:19 -0600

    implementando dos ejercicios con explicit y implicit wait

--- robot_10_impicit_wait.py
+++ robot_10_impicit_wait.py
@@ -2,25 +2,22 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 
-
 class usando_unittest(unittest.TestCase):
+
     def setUp(self):
-        service = Service(executable_path='c:\cdChrome\chromedriver.exe')
+        service = Service(executable_path='c:\cdDriver\chromedriver')
         options = webdriver.ChromeOptions()
+        options.add_experimental_option("excludeSwitches",['enable-automation'])
         self.driver = webdriver.Chrome(service=service, options=options)
 
-    def test_usando_Explicit_wait(self):
+    def test_usando_implicit_wait(self):
         driver = self.driver
+        driver.implicitly_wait(5)
         driver.get("https://www.google.com")
-        try:
-            element = WebDriverWait(driver, 100).until(ec.presence_of_all_elements_located((By.NAME, "q")))
-        finally:
-            driver.quit()
-
+        myDynamicElement = driver.find_element(By.NAME,"q")
+    
 if __name__ == '__main__':
     unittest.main()
-
-    
